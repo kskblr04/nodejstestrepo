dabd450ba645c049696f5d8a36513e0d43bba9f3
sanjiv <sanjiv.gmat@gmail.com>
2020-11-11 14:44:07 +0000

    explicit action to commit

--- hello.js
+++ hello.js
@@ -1,1 +1,4 @@
-console.log("Hello universe")
+const messageModule = require("./messages")
+let myVar = "universe!"
+console.log("Hello %s", myVar)
+messageModule.messages.forEach(m => console.log(m));
---
+++ messages.js
@@ -0,0 +1,2 @@
+"use strict"
+exports.messages = ["Hello.","This","is","an","array","of","strings"];
