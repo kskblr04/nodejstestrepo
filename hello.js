b4d48086bd0ef9328d5d5c5f0e89ca6c396dc698
sanjiv <sanjiv.gmat@gmail.com>
2021-04-04 08:30:58 +0000

    routing

--- hello.js
+++ hello.js
@@ -11,18 +11,29 @@ console.log(myCity);
 const addme = require("./addme");
 console.log(addme.addNum(3,4));
 
+const routeResponseMap = {
+    "/info": "<h1>Info Page</h1>",  
+    "/contact": "<h1>Contact Us</h1>",  
+    "/about": "<h1>Learn More About Us.</h1>",  
+    "/hello": "<h1>Say hello by emailing us here</h1>",  
+    "/error": "<h1>Sorry the page you are looking for is not here.</h1>"
+};
+
 const port = 3000, 
 http = require("http"),                                           
 httpStatus = require("http-status-codes"),  
-app = http.createServer((request, response) => {                  
+app = http.createServer((req, res) => {                  
     console.log("Received an incoming request!");    
-    response.writeHead(httpStatus.OK, {      
+    res.writeHead(httpStatus.OK, {      
         "Content-Type": "text/html"    
     });                                                             
-    let responseMessage = "<h1>Hello, Universe!</h1>";    
-    response.write(responseMessage);    
-    response.end();    
-    console.log(`Sent a response : ${responseMessage}`);  
+    if (routeResponseMap[req.url]) {
+        res.write(routeResponseMap[req.url]);    
+    } else {      
+        res.write("<h1>Welcome!</h1>");                     
+    }    
+    res.end();    
+    console.log(`Sent a response : ${res}`);  
 });
 app.listen(port);
 console.log(`The server has started and is listening on port number:${port}`);
